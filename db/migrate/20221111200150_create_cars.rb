2d1e8f797a7286e17e684faa172635f274a431f7
crisandryj <crisandryj@gmail.com>
2022-11-11 15:10:26 -0500

    created car and variant model

--- db/migrate/20221111200150_create_cars.rb
+++ db/migrate/20221111200150_create_cars.rb
@@ -1,7 +1,10 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-
+      t.string :name
+      t.string :model
+      t.integer :year
+      
       t.timestamps
     end
   end
