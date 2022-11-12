405de551de7cb6bf951e86707a35845d7c77c94d
crisandryj <crisandryj@gmail.com>
2022-11-12 12:13:28 -0500

    update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,24 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.save
+    if @car.save
+      redirect_to root_path
+    end
+    # raise params.inspect
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :year, :name)
+  end
 end
